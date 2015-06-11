a2d87fcb7067af3e80f0cf0b8c6143c65b3a7e79
JcBenard <niteburn@hotmail.com>
2015-06-11 10:04:54 -0400

    Created base fles from template

---
+++ comp2007-assign1/comp2007-assign1/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace comp2007_assign1.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ comp2007-assign1/comp2007-assign1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(comp2007_assign1.Startup))]
+namespace comp2007_assign1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
